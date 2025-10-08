81f934b309304a3f234cca8a789fa35fbb0237c6
Philedo🦖 <seahsongli@gmail.com>
2025-10-08 22:11:52 +0800

    Update endpoint to fetch options data.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <cstdlib>
 #include <iostream>
 #include <filesystem>
+#include <format>
+#include "nlohmann/json.hpp"
 #include "DataFetcher.h"
 
 void loadEnvFile(const std::string& filename) {
@@ -11,7 +13,7 @@ void loadEnvFile(const std::string& filename) {
         std::cerr << "Error: Could not open .env file: " << filename << std::endl;
         return;
     }
-
+    // to load the env file
     std::string line;
     while (std::getline(file, line)) {
         size_t equalsPos = line.find('=');
@@ -34,12 +36,37 @@ void loadEnvFile(const std::string& filename) {
 }
 
 int main() {
-    std::cout << "Current working directory: " << std::filesystem::current_path() << "\n";
+    //std::cout << "Current working directory: " << std::filesystem::current_path() << "\n";
     loadEnvFile(".env");
 
 	DataFetcher dataFetcher{};
     std::string apiKey = getenv("API_KEY");
-	std::string url = "https://api.polygon.io/v3/reference/dividends?apiKey=" + apiKey;
+    std::string underlying_ticker = "ES";
+	/*std::string url = "https://api.polygon.io/v3/reference/options/contracts?apiKey=" + apiKey;*/
+    std::string url = std::format("https://api.polygon.io/v3/reference/options/contracts?underlying_ticker={}&apiKey={}", underlying_ticker, apiKey);
 	std::string response = dataFetcher.fetchUrl(url);
 	std::cout << response;
+
+    try {
+        nlohmann::json option_data = nlohmann::json::parse(response);
+        // while there is more data to be fetched
+        while (option_data["next_url"] != "") {
+            std::string next_page_url = std::format("{}&underlying_ticker={}&apiKey={}", option_data["next_url"].get<std::string>(), underlying_ticker, apiKey);
+            std::string next_page = dataFetcher.fetchUrl(next_page_url);
+            std::cout << next_page;
+            try {
+                nlohmann::json next_option_data = nlohmann::json::parse(next_page);
+                // To do: Determine how to store the data, do we put it in a data structure? 
+            }
+            catch (const nlohmann::json::parse_error& e) {
+                std::cerr << "JSON parse error: " << e.what() << std::endl;
+            }
+
+        }
+        // After fetching all the data, we should do something like taking in all the prices of the option and churn out IV. (Black scholes)
+        // Plot market IV against Option chain IV.
+    } catch (const nlohmann::json::parse_error& e) {
+        std::cerr << "JSON parse error: " << e.what() << std::endl;
+    }
+    return 0;
 }
